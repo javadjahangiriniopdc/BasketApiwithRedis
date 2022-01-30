7b4bc0c773b75df478c72a50abbb21ab2c755e82
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-01-30 03:09:55 -0800

    add Controllers

---
+++ BasketApiwithRedis/Controllers/BasketController.cs
@@ -0,0 +1,29 @@
+﻿using BasketApiwithRedis.Entities;
+using BasketApiwithRedis.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BasketApiwithRedis.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController:ControllerBase
+    {
+        private readonly IBasketRepository _repositroy;
+        public BasketController(IBasketRepository repositroy)
+        {
+            _repositroy = repositroy;
+        }
+        [HttpGet("{usserName",Name ="GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),(int) HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket=await _repositroy.GetBasket(userName);
+            return Ok(basket ??  new ShoppingCart(userName));
+        }
+
+
+
+    } 
+}
