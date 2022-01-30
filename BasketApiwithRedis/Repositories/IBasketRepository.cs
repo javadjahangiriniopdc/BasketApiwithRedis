e96f15dbe8d7d98230b4e9405843c64d3f22ecfb
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-01-29 23:32:23 -0800

    Add Repostiry

---
+++ BasketApiwithRedis/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using BasketApiwithRedis.Entities;
+using System.Threading.Tasks;
+
+namespace BasketApiwithRedis.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
